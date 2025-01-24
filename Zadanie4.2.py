dc85586433452326290eef5c6d30be351623f0db
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-01-24 19:07:05 +0100

    Update print

--- Zadanie4.2.py
+++ Zadanie4.2.py
@@ -1,4 +1,4 @@
 def check_if_palindrome(word):
     return word == word[::-1]
 word = input("Please insert a word: ")
-print(check_if_palindrome(word))
+print(f"Is your word a palindrome? {check_if_palindrome(word)}")
