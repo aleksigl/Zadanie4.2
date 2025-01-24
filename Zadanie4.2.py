6d97093f17d7f023087c900269a4523b8f988cb9
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-01-24 19:12:50 +0100

    Add docstring

--- Zadanie4.2.py
+++ Zadanie4.2.py
@@ -1,4 +1,11 @@
 def check_if_palindrome(word):
+    """
+    Checks whether inserted word is a palindrome
+    Aruments:
+    word - a string inserted by user, to be chcecked whether a palindrome
+    Return:
+    A boolean value describing if a word is a palindrome
+    """
     return word == word[::-1]
 word = input("Please insert a word: ")
 print(f"Is your word a palindrome? {check_if_palindrome(word)}")
